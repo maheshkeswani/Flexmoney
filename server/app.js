c26bdabfb77ce7afb24796f235b73af7fdd144ff
maheshkeswani <maheshkeswani920@gmail.com>
2022-12-12 14:32:21 +0530

    removed-env

--- server/app.js
+++ server/app.js
@@ -12,6 +12,6 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/",user);
-const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+app.listen(process.env.PORT|| 5000, () => console.log(`Server is running on ${process.env.PORT||5000}`));
